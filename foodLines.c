844b0fdfea13021e8f96ecf3ecc0237864ada960
Gary Smith <goose200782@gmail.com>
2021-04-27 16:37:07 -0500

    Added actual c program file. Starting my journey to learn C and algorithms

---
+++ foodLines.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_LINES 100
+
+int main(void) {
+  int lines[MAX_LINES];
+  int n, m, i;
+  scanf("%d%d", )
+
+  return 0;
+}
